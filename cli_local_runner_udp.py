f794cdee4cae83425543889a3a766e96f54f9745
Dein GitHub-Benutzername <ayoub.bouda@stud.fra-uas.de>
2025-05-25 18:23:51 +0200

    Test

--- cli_local_runner_udp.py
+++ cli_local_runner_udp.py
@@ -113,6 +113,8 @@ def main():
         # Nachricht an den Zielport senden
         sock.sendto(full_msg.encode("utf-8"), ("127.0.0.1", target_port))
 
+
+
 # Einstiegspunkt bei Direktaufruf der Datei
 if __name__ == "__main__":
     main()
